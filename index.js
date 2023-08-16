4435a1115926cae8c81b0629ea20eec428b2e907
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-08-16 14:56:54 -0300

    feat: simple query with prisma

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const bp = require("body-parser")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")))
 app.use(bp.urlencoded({ extended: false }))
 app.use(bp.json())
+
+const dbConnect = async () => {
+    await prisma.$connect()
+    console.log("Connected to database")
+}
 
 const questions = []
 
@@ -19,18 +26,26 @@ app.get("/ask", (req, res) => {
     res.render("ask-form")
 })
 
-app.post("/ask", (req, res) => {
+app.post("/ask", async (req, res) => {
     questions.push({
         title: req.body.title,
         description: req.body.description
     })
 
-    console.log(questions)
+    await prisma.question.create({
+        data: {
+            title: req.body.title,
+            description: req.body.description
+        }
+    })
+
+    console.log(await prisma.question.findMany())
 
     res.redirect("/ask/")
 })
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await dbConnect()
     console.log("Server running on port 3000")
 })
 
