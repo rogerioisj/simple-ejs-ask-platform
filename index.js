fbae69ff6b739f84c3f17d3c0a567c5482cd01cf
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-08-16 14:07:00 -0300

    feat: add route to save a question

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const bp = require("body-parser")
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")))
+app.use(bp.urlencoded({ extended: false }))
+app.use(bp.json())
+
+const questions = []
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -12,6 +17,17 @@ app.get("/", (req, res) => {
 
 app.get("/ask", (req, res) => {
     res.render("ask-form")
+})
+
+app.post("/ask", (req, res) => {
+    questions.push({
+        title: req.body.title,
+        description: req.body.description
+    })
+
+    console.log(questions)
+
+    res.redirect("/ask/")
 })
 
 app.listen(3000, () => {
